e6d5933c5be4b0f010d41cb39d96869384e8f1de
Leo <Leo@DESKTOP-LU2I4O1>
2022-07-06 19:02:38 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios-Prova-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios_Prova_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //4 – Utilizando estrutura FOR, escrever um programa para exibir os números de 1 até 50 na tela.
+            int a;
+            for (a = 1; a < 51; a++)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
